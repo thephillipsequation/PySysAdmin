280931ebbd3f16396ad90d5196df55e3739e61b6
Josh Phillips <joshua.andrew.phillips@gmail.com>
2017-02-22 12:04:00 -0500

    fixed fabfile name

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,10 @@
-from fabric.api import run
+import sys
+import os
+from fabric.api import env, local, sudo, require, run, put, settings, cd, lcd
 
-def host_type():
-    run('uname -s')
+
+def _ensure_virtualenv():
+    if "VIRTUAL_ENV" not in os.environ:
+        sys.stderr.write("$VIRTUAL_ENV not found. Make sure to activate virtualenv first\n\n")
+        sys.exit(-1)
+    env.virtualenv = os.environ["VIRTUAL_ENV"]
